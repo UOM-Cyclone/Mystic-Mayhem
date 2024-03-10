25f3c6110b33375580ecb404f433e79439272633
Thumul <thumul.dasun@gmail.com>
2024-03-10 17:31:31 +0530

    Implement Player class

--- MysticMayhem/Player.java
+++ MysticMayhem/Player.java
@@ -1,11 +1,18 @@
 package MysticMayhem;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import MysticMayhem.Grounds.Ground;
 
 public class Player {
     private static int count = 0;
+    static Map<String,Player> players = new HashMap<>();
+    static ArrayList<String> usernames = new ArrayList<>();
     private int gc, xp, uID;
     private String username, pwd, name;
-    Ground hg;
+    private Ground hg;
+
     public Player(String name, String uName, String pwd){
         this.name = name;
         this.username = uName;
@@ -14,9 +21,64 @@ public class Player {
         xp = 0;
         count++;
         uID  =  count;
+        usernames.add(uName);
+
+        players.put(uName, this);
     }
 
     Player(String name,String uName){
         
+    }
+
+    public void updateHashMap(){
+        players.put(this.username, this);
+    }
+
+    public String getUserName(){
+        return this.username;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getGC() {
+        return this.gc;
+    }
+
+    public int getXP(){
+        return this.xp;
+    }
+
+    public String getPwd(){
+        return this.pwd;
+    }
+
+    public Ground getHomeGround(){
+        return this.hg;
+    }
+
+    public void increaseXP(int i){
+        this.xp += i;
+    }
+
+    public void changeGC(int amount){
+        this.gc += amount;
+    }
+
+    public void chnagePwd(String newPwd){
+        this.pwd = newPwd;
+    }
+
+    public void changeUserName(String newUName){
+        this.username = newUName;
+    }
+
+    public void changeName(String newName){
+        this.name = newName;
+    }
+
+    public void setHomeGround(Ground ground){
+        this.hg = ground;
     }
 }
